da132fea437b1da3ba23a34c88e0239a90e1f831
angela <obva.dev@gmail.com>
2026-01-18 22:05:24 +0300

    minor fix mix.exs

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule FpLab2.MixProject do
 
   def project do
     [
-      app: :fp-laboratory-2,
+      app: :fpLab2,
       version: "0.1.0",
       elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
